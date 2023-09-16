7d3a161772ebaf71477ab92b0fc5e7c523191817
ar2rworld <ar2r.world@gmail.com>
2023-09-16 12:44:49 -0400

    data for project

---
+++ src/routes/projects/data.js
@@ -0,0 +1,68 @@
+export const projects = [
+  {
+    id: "1",
+    name: "NemoBot",
+    scr_url: "https://github.com/ar2rworld/NemoBot",
+    url: "https://t.me/nemo4_bot",
+    description: "Some long text",
+    technologies: [
+      {
+        name: "python",
+        alt: "python: snake language",
+        img: "python.png"
+      },
+      { 
+        name: "redis",
+        alt: "redis: the super key value storage",
+        img: "redis.png"
+      },
+      {
+        name: "mongodb",
+        alt: "mongodb: nosql database",
+        img: "mongodb.png"
+      },
+    ]
+  },
+  {
+    id: "2",
+    name: "OsyNe telegram video downloader",
+    scr_url: "https://github.com/ar2rworld/osyne-telegram-video-downloader",
+    url: "https://t.me/dih_video_downloader_bot",
+    description: "Some long text",
+    technologies: [ 
+      {
+        name: "golang",
+        alt: "golang: c like compiled language",
+        img: "golang.png"
+      }
+    ]
+  },
+  {
+    id: "3",
+    name: "Pet Sitter Service",
+    scr_url: "https://github.com/hatchways/team-chicken-noodle",
+    description: "Now a days, pet owner have to struggle a lot to find some one to sit their pet who is trust worthy and reliable. In order to solve this problem our team have create a wonder full solution called Loving Sitter. By using our application, pet owners can easily search for pet sitter in their city and enjoy their services.",
+    technologies: [ 
+      {
+        name: "nodejs",
+        alt: "nodejs: if frontend dev wants to do some backend",
+        img: "nodejs.png"
+      },
+      {
+        name: "mongodb",
+        alt: "mongodb: nosql database",
+        img: "mongodb.png"
+      },
+      {
+        name: "reactjs",
+        alt: "reactjs: facebook created reactive frontend framework",
+        img: "reactjs.png"
+      },
+      {
+        name: "typescript",
+        alt: "typescript: javascript with types",
+        img: "typescript.png"
+      }
+    ]
+  },
+];
