93c74fbb9be379ff9c23aac2f3faf2bb627c8ea9
ar2rworld <ar2r.world@gmail.com>
2023-09-17 17:46:02 -0400

    personal info in RevealedPhrases

--- src/stores/RevealedStore.ts
+++ src/stores/RevealedStore.ts
@@ -2,10 +2,16 @@ import { writable } from 'svelte/store';
 
 export const RevealedStore = writable({
   LIFE: false,
-  HOMEINFO: false
+  HOMEINFO: false,
+  GITHUB: false,
+  EMAIL: false,
+  LINKEDIN: false
 });
 
 export const RevealedPhrases = {
   LIFE: "life and a special day today!",
-  HOMEINFO: "some projects that Artur developed"
+  HOMEINFO: "some projects that Artur developed",
+  GITHUB: "https://github.com/ar2rworld",
+  EMAIL: "ar2r(dot)world(at)gmail(dot)com",
+  LINKEDIN: "https://www.linkedin.com/in/artur-linnik-ba42b4192/"
 }
