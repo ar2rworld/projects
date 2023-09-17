93b7a8453172c15382b231641581d5aaec31fbb6
ar2rworld <ar2r.world@gmail.com>
2023-09-17 13:57:38 -0400

    Project page

--- src/routes/projects/[slug]/+page.svelte
+++ src/routes/projects/[slug]/+page.svelte
@@ -1,6 +1,53 @@
-<script>
+<script lang="ts">
 	export let data;
+  const { project } = data;
 </script>
 
-<h1>{data.project.name}</h1>
+<div class="root">
+  <h1>{project.name}</h1>
+  
+  {#if project.url}
+    <a href={project.url}>URL</a>
+  {:else}
+    <p>Is not available publically</p>
+  {/if}
 
+  <p>You can find sources <a href={project.src_url}>here</a></p>
+
+  <p>{project.description}</p>
+
+  {#if project.technologies}
+    <h4>Built with:</h4>
+    <div class="technologiesGrid">
+      {#each project.technologies as p}
+        <div class="technologyItem">
+            <img src={"/" + p.img} alt={p.alt}/>
+            <p>{p.name}</p>
+        </div>
+      {/each}
+    </div>
+  {/if}
+</div>
+
+<style>
+  div.root {
+    margin: 0px 3%;
+    padding: 1px 2%;
+    /* background-color: var(--bg-3); */
+    border-radius: var(--border-radius);
+    border: 1px solid var(--fg-3);
+  }
+  img {
+    width: 30%;
+    /* widows: 100px;  */
+  }
+  div.technologiesGrid {
+    display: grid;
+    grid-auto-flow: column;
+    align-items: center;
+    justify-content: space-evenly;
+  }
+  div.technologyItem {
+    text-align: center;
+  }
+</style>
