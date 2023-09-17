4a619d265ec8c109324333b378e387aa6a16e42a
ar2rworld <ar2r.world@gmail.com>
2023-09-17 13:58:33 -0400

    moved css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,10 @@
+<div>
+  <h1>Hello!</h1>
+  <h2>I hope that you will have a good day today</h2>
+</div>
+
 <style>
   div {
     text-align: center;
   }
 </style>
-
-<div>
-  <h1>Hello!</h1>
-  <h2>I hope that you will have a good day today</h2>
-</div>
