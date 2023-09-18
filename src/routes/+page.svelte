9fd5c829b795053d0c782c08a681f50555a00d42
ar2rworld <ar2r.world@gmail.com>
2023-09-18 14:58:19 -0400

    handleReveal

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,5 @@
 <script lang="ts">
-  import { RevealedPhrases, RevealedStore } from "../stores/RevealedStore";
-  const handleLifeReveal = () => {
-    $RevealedStore.LIFE = true;
-  }
-  const handleHomeInfoReveal = () => {
-    $RevealedStore.HOMEINFO = true;
-  }
+  import { RevealedPhrases, RevealedStore, RevealedStoreKeysE, handleReveal } from "../stores/RevealedStore";
 </script>
 
 <div>
@@ -14,8 +8,8 @@
   <h2>My name is Artur Linnik</h2>
   <h2>But you can call me Artur</h2>
   <br>
-  <h3 on:mouseenter={handleLifeReveal}>I hope that you will have a good { $RevealedStore.LIFE ? RevealedPhrases.LIFE : "..." }</h3>
-  <h3 on:mouseenter={handleHomeInfoReveal}>If you click on the "projects" tab you will find { $RevealedStore.HOMEINFO ? RevealedPhrases.HOMEINFO : "...." }</h3>
+  <h3 on:mouseenter={() => handleReveal(RevealedStoreKeysE.LIFE)}>I hope that you will have a good { $RevealedStore.LIFE ? RevealedPhrases.LIFE : "..." }</h3>
+  <h3 on:mouseenter={() => handleReveal(RevealedStoreKeysE.HOMEINFO)}>If you click on the "projects" tab you will find { $RevealedStore.HOMEINFO ? RevealedPhrases.HOMEINFO : "...." }</h3>
 </div>
 
 <style>
--- src/stores/RevealedStore.ts
+++ src/stores/RevealedStore.ts
@@ -1,12 +1,51 @@
 import { writable } from 'svelte/store';
 
+const RevealedStoreKeys = {
+  LIFE     : 0,
+  HOMEINFO : 1,
+  GITHUB   : 2,
+  EMAIL    : 3,
+  LINKEDIN : 4
+}
+
+export enum RevealedStoreKeysE { 
+  LIFE     = RevealedStoreKeys.LIFE,
+  HOMEINFO = RevealedStoreKeys.HOMEINFO,
+  GITHUB   = RevealedStoreKeys.GITHUB,
+  EMAIL    = RevealedStoreKeys.EMAIL,
+  LINKEDIN = RevealedStoreKeys.LINKEDIN
+}
+
 export const RevealedStore = writable({
   LIFE: false,
   HOMEINFO: false,
   GITHUB: false,
   EMAIL: false,
-  LINKEDIN: false
+  LINKEDIN: false,
 });
+
+export const handleReveal = (key: RevealedStoreKeysE) => {
+  RevealedStore.update(s => {
+    switch (key) {
+      case RevealedStoreKeysE.LIFE:
+        s.LIFE = true;
+        break;
+      case RevealedStoreKeysE.HOMEINFO:
+        s.HOMEINFO = true;
+        break;
+      case RevealedStoreKeysE.GITHUB:
+        s.GITHUB = true;
+        break;
+      case RevealedStoreKeysE.EMAIL:
+        s.EMAIL = true;
+        break;
+      case RevealedStoreKeysE.LINKEDIN:
+        s.LINKEDIN = true;
+        break;
+    }
+    return s;
+  })
+}
 
 export const RevealedPhrases = {
   LIFE: "life and a special day today!",
