4dfce15a2f360e78a5464b28cb02b2418d53a361
ar2rworld <ar2r.world@gmail.com>
2023-12-18 04:44:12 -0500

    loginTokens store

---
+++ src/stores/loginTokens.ts
@@ -0,0 +1,10 @@
+import { writable } from 'svelte/store';
+import type { ILoginTokens } from '../types/login';
+
+const loginTokens: ILoginTokens = {
+  accessToken: "",
+  refreshToken: "",
+  expiresIn: 0,
+};
+
+export const LoginTokens = writable(loginTokens);
---
+++ src/types/login.ts
@@ -0,0 +1,5 @@
+export interface ILoginTokens {
+  accessToken: string;
+  refreshToken: string;
+  expiresIn: number;
+};
