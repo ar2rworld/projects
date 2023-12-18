79b86f24cb4dfa1bcb0a6858dca30628767918cf
ar2rworld <ar2r.world@gmail.com>
2023-12-18 04:43:27 -0500

    login page

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,55 +1,106 @@
 <script lang="ts">
-  let username: string = "";
-  let password: string = "";
+  import LayoutGrid, { Cell } from '@smui/layout-grid';
+  import Button, { Label } from '@smui/button';
+  import Textfield from '@smui/textfield';
 
-  const baseUrl = "https://192.18.155.170/projectsapi";
+  import axios, { Axios, AxiosError } from 'axios';
+  import type { ILoginTokens } from '../../types/login';
+	import { LoginTokens } from '../../stores/loginTokens';
 
-  const handleSubmit = async () => {  
-    const data = {
-      username,
-      password
-    };
+  let username: string = "a";
+  let password: string = "dmin";
 
-    fetch(baseUrl + "/login",
-      {
-        method: "POST",
-        body: JSON.stringify(data)
+  let message = "";
+  let showMessage = false;
+  const baseUrl = "http://localhost:8050/projectsapi";
+
+  let error = "";
+
+  const handleSubmit = async () => {
+    error = "";
+    message = "";
+
+    const data = { username, password };
+
+    axios.post(baseUrl + "/login", data)
+    .then(d => {
+      const loginTokens = d.data as ILoginTokens;
+      LoginTokens.set(loginTokens);
+      console.log(loginTokens);
+    })
+    .catch(e => {
+      console.log(e)
+      if ( e instanceof axios.AxiosError ) {
+        message = e.response?.data?.message
+        switch(e.code){
+          case "ERR_NETWORK":
+            error = "Sorry, backend is currently down &#128556"
+          break;
+          case "ERR_BAD_REQUEST":
+            error = "Looks like your credentials are incorrect &#128517"
+          break;
+          default: {
+            error = "some other exception"
+          }
+        } 
       }
-    )
-    .then(v => console.log(v.json()))
-    .catch(e => console.log(e));
+    })
   }
 
 </script>
 
-<div>
-  <form on:submit|preventDefault={handleSubmit}>
-    <input placeholder="username" type="text" bind:value={username} required />
-    <input placeholder="password" type="password" bind:value={password} required />
-
-    <button type="submit">
-      login
-    </button>
-  </form>
-</div>
+<form on:submit|preventDefault={handleSubmit}>
+  <LayoutGrid cols="3" row-gap="10px">
+    <Cell span="4">
+      { #if error != "" }
+        <p
+          on:mouseover={ () => showMessage = true }
+          on:mouseleave={ () => showMessage = false }
+          on:focus={ () => showMessage = true }
+        >
+          {@html error}
+        </p>
+      { /if }
+    </Cell>
+    <Cell span="4">
+      <div class="centered-container">
+        <div class="item">
+          <Textfield label="username" type="text" bind:value={username} required />
+        </div>
+        <div class="item">
+          <Textfield label="password" type="password" bind:value={password} required />
+        </div>
+        <div class="item">
+          <Button color="primary" variant="raised" type="submit">
+            <Label>login</Label>
+          </Button>
+        </div>
+      </div>
+    </Cell>
+    <Cell span="4">
+      { #if showMessage || true }
+          <p>{@html message}</p>
+      { /if }
+    </Cell>
+  </LayoutGrid>
+</form>
 
 <style>
   form {
-    margin: auto;
-  }
-
-  input {
     padding: 0.5%;
+    background-color: var(--bg-2);
     color: var(--fg-1);
-    background: var(--bg-3);
-    border-color: var(--bg-2);
+    margin: 1% 5%;
   }
 
-  button {
-    padding: 0.5%;
-    margin: auto;
-    border: none;
-    color: var(--fg-2);
-    background: var(--bg-3);
+  .centered-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+  }
+  .item {
+    margin-bottom: 0.5%;
   }
 </style>
