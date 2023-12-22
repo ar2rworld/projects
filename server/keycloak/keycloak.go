5c3d7021b193beb90071a76fdb29c8f0baaf8402
ar2rworld <ar2r.world@gmail.com>
2023-12-22 18:16:29 -0500

    add KEYCLOAK_HOST to env

--- server/keycloak/keycloak.go
+++ server/keycloak/keycloak.go
@@ -18,13 +18,19 @@ func NewKeycloak() (*Keycloak, error) {
 	id := os.Getenv("KEYCLOAK_CLIENT_ID")
 	secret := os.Getenv("KEYCLOAK_CLIENT_SECRET")
 	realm := os.Getenv("KEYCLOAK_REALM")
+	kcHost := os.Getenv("KEYCLOAK_HOST")
 
-	if id == "" || secret == "" || realm == "" {
-		return nil, fmt.Errorf("missing keycloak environment variables: KEYCLOAK_CLIENT_ID or KEYCLOAK_CLIENT_SECRET or KEYCLOAK_REALM")
+	if id == "" || secret == "" || realm == "" || kcHost == ""  {
+		return nil, fmt.Errorf(`missing keycloak environment variables:
+		KEYCLOAK_CLIENT_ID=%s or
+		KEYCLOAK_CLIENT_SECRET=%s or
+		KEYCLOAK_REALM=%s or
+		KEYCLOAK_HOST=%s
+		`, id, secret, realm, kcHost)
 	}
 
 	return &Keycloak{
-		Gocloak:      *gocloak.NewClient("http://localhost:8080"),
+		Gocloak:      *gocloak.NewClient(kcHost),
 		ClientId:     id,
 		ClientSecret: secret,
 		Realm:        realm,
