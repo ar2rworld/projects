5e05a79b8e36d0bbffad86de791e6f82645fee6f
ar2rworld <ar2r.world@gmail.com>
2024-03-07 04:17:27 -0500

    improve contact page

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,16 +1,33 @@
 <script lang="ts">
-  let name = "a";
-  let email = "a@gmjk.a";
-  let message = "heillo";
+  let name = "";
+  let email = "";
+  let message = "";
+
+  let output = "";
+
+  const reset = () => { name = ""; email = ""; message = ""; }
 
   const handleSubmit = (e: SubmitEvent) => {
     e.preventDefault()
+    output = "";
     console.log(name, email, message);
     fetch("/api/contact", {
       method: "POST",
       body: JSON.stringify({
         name, email, message
       })
+    })
+    .then((e:Response) => {
+      console.log(e);
+      if ( e.status != 200 ) {
+        output = "Something went wrong("
+        return;
+      }
+      reset();
+      output = "Got it!"
+    })
+    .catch(() => {
+      output = "Something went wrong((("
     })
   }
 </script>
@@ -21,4 +38,5 @@
   <div><p>Email:</p><input class="bg-bg-3 dark:bg-d-bg-3" type="email" bind:value={email} required/></div>
   <div><p>Message:</p><input class="bg-bg-3 dark:bg-d-bg-3" type="text" bind:value={message} required/></div>
   <input type="submit" value="send" />
+  {output ? output : ""}
 </form>
