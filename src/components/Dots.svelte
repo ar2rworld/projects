c592d6e8d14eed9d838ebfc05ceeba827b40f580
ar2rworld <ar2r.world@gmail.com>
2024-07-04 22:11:18 -0400

    add Dots

---
+++ src/components/Dots.svelte
@@ -0,0 +1,22 @@
+<script>
+	import { onDestroy } from "svelte";
+  
+  let randomNumber = Math.floor(Math.random()* 2 + 3 ); // [3..5]
+  let counter = 1;
+  let dots = "."
+  let interval = setInterval(() => {
+    counter += 1;
+    dots += ".";
+    if ( counter > randomNumber ) {
+      counter = 1;
+      dots = ".";
+    }
+  }, 1000);
+
+  onDestroy(() => {
+    clearInterval(interval);
+  })
+
+</script>
+
+{ dots }
