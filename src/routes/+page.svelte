5e3c3ba80dc34788cc14f7f9ae57a3b61e82df73
ar2rworld <ar2r.world@gmail.com>
2024-07-04 23:01:56 -0400

    hint added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,25 @@
 <script lang="ts">
+	import { fade } from 'svelte/transition';
+
 	import Dots from '../components/Dots.svelte';
-import {
+	import {
 		RevealedPhrases,
 		RevealedStore,
 		RevealedStoreKeysE,
 		handleReveal
 	} from '../stores/RevealedStore';
+
+	let showHint = true;
+	const hideHint = () => {
+		showHint = false;
+	}	
 </script>
 
 <div class="max-w-xl mx-auto">
 	<h1>Hello!</h1>
+	{ #if showHint }
+		<h2 class="bg-fg-2 rounded-sm" out:fade on:mouseenter={() => hideHint()}>Hey, this is interactive page, so try hovering over <Dots/> and see what happens</h2>
+	{ /if }
 	<h2>And welcome to the Artur Linnik's projects website</h2>
 	<h2>My name is Artur Linnik</h2>
 	<h2>But you can call me Artur</h2>
