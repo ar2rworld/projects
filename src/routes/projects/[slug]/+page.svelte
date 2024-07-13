58c6632f6c7470c9627ebea3eca2b012eb339be4
ar2rworld <ar2r.world@gmail.com>
2024-07-13 16:55:13 -0700

    added logo to project[slug]

--- src/routes/projects/[slug]/+page.svelte
+++ src/routes/projects/[slug]/+page.svelte
@@ -6,31 +6,38 @@
 <div class="root">
 	<h1>{project.name}</h1>
 
-	{#if project.url}
-		<a href={project.url}>URL</a>
-	{:else}
-		<p>Is not available publically</p>
-	{/if}
+	<div class="grid grid-cols-1 sm:grid-cols-2 gap-4">
+		<div>
+			{#if project.url}
+				<a href={project.url}>URL</a>
+			{:else}
+				<p>Is not available publically</p>
+			{/if}
 
-	<p>You can find sources <a href={project.src_url}>here</a></p>
+			<p>You can find sources <a href={project.src_url}>here</a></p>
 
-	{#if project.descriptionHTML != undefined}
-		{@html project.descriptionHTML}
-	{:else}
-		<p>{project.description}</p>
-	{/if}
-
-	{#if project.technologies}
-		<h4>Built with:</h4>
-		<div class="technologiesGrid">
-			{#each project.technologies as p}
-				<div class="technologyItem">
-					<img src={'/' + p.img} alt={p.alt} />
-					<p>{p.name}</p>
-				</div>
-			{/each}
+			{#if project.descriptionHTML != undefined}
+				{@html project.descriptionHTML}
+			{:else}
+				<p>{project.description}</p>
+			{/if}
 		</div>
-	{/if}
+		<div>
+			<img class="mx-auto min-h-2 rounded-sm border-2 border-fg-2" src="/{project.logo}" alt="{project.name} project logo" />
+			{#if project.technologies}
+				<h4>Built with:</h4>
+				<div class="technologiesGrid">
+					{#each project.technologies as p}
+						<div class="technologyItem">
+							<img src={'/' + p.img} alt={p.alt} />
+							<p>{p.name}</p>
+						</div>
+					{/each}
+				</div>
+			{/if}
+		</div>
+	</div>
+
 </div>
 
 <style>
