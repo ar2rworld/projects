3cc9194bc4693b6fc09ff6569df68a09dbbcfb9f
ar2rworld <ar2r.world@gmail.com>
2024-07-13 18:32:30 -0700

    add translation to technologies

--- src/routes/projects/[slug]/+page.svelte
+++ src/routes/projects/[slug]/+page.svelte
@@ -1,10 +1,21 @@
 <script lang="ts">
+	import { fade, fly } from 'svelte/transition';
+
 	export let data;
 	const { project } = data;
+
+	let focusedName: string | null = "";
+
+	const onMouseEnter = (name: string) => {
+		focusedName = name;
+	}
+	const onMouseLeave = () => {
+		focusedName = null;
+	}
 </script>
 
 <div class="root">
-	<h1>{project.name}</h1>
+	<h1 class="underline">{project.name}</h1>
 
 	<div class="grid grid-cols-1 sm:grid-cols-2 gap-4">
 		<div>
@@ -23,14 +34,21 @@
 			{/if}
 		</div>
 		<div>
-			<img class="mx-auto min-h-2 rounded-sm border-2 border-fg-2" src="/{project.logo}" alt="{project.name} project logo" />
+			<a href="{project.url}" target="_blank"><img class="mx-auto min-h-2 rounded-sm border-2 border-fg-2" src="/{project.logo}" alt="{project.name} project logo" /></a>
 			{#if project.technologies}
 				<h4>Built with:</h4>
 				<div class="technologiesGrid">
 					{#each project.technologies as p}
 						<div class="technologyItem">
-							<img src={'/' + p.img} alt={p.alt} />
-							<p>{p.name}</p>
+							<img
+								src={'/' + p.img}
+								alt={p.alt}
+								on:mouseenter={() => { onMouseEnter(p.name) }}
+								on:mouseleave={() => { onMouseLeave() }}
+							/>
+							{ #if p.name == focusedName }
+								<p class="ml-auto opacity-75" in:fly={{ y: 100, duration: 1000 }} out:fade>{p.name}</p>
+							{ /if }
 						</div>
 					{/each}
 				</div>
