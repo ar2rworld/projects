a5198b71f3b433b96fdb8cb0ee4ccdde1a191a90
ar2rworld <ar2r.world@gmail.com>
2024-07-15 21:42:44 -0700

    add character and max/min to Dots component

--- src/components/Dots.svelte
+++ src/components/Dots.svelte
@@ -1,15 +1,23 @@
-<script>
+<script lang="ts">
 	import { onDestroy } from "svelte";
+
+  export let character: string = ".";
+  export let min: number = 2;
+  export let max: number = 5;
+
+  if (max < min) {
+    console.warn("why did you enter max < min?")
+  }
   
-  let randomNumber = Math.floor(Math.random()* 2 + 3 ); // [3..5]
+  let randomNumber = Math.round(Math.random() * min + (max - min) ); // [3..5]
   let counter = 1;
-  let dots = "."
+  let dots = character;
   let interval = setInterval(() => {
     counter += 1;
-    dots += ".";
+    dots += character;
     if ( counter > randomNumber ) {
       counter = 1;
-      dots = ".";
+      dots = character;
     }
   }, 1000);
 
