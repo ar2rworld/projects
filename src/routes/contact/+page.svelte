4e9cbb523d0ea6fa039fe8afbd23b1c8e0d78483
ar2rworld <ar2r.world@gmail.com>
2024-07-15 22:42:17 -0700

    add transition to contact showHow

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { onDestroy } from "svelte";
 	import Dots from "../../components/Dots.svelte";
+	import { fade, slide } from "svelte/transition";
 
   let name = "";
   let email = "";
@@ -52,9 +53,9 @@
     <div class="my-1"><p class="text-left">Email:</p><input class="w-full bg-bg-3 dark:bg-d-bg-3" type="email" bind:value={email} required/></div>
     <div class="my-1"><p class="text-left">Message:</p><textarea class="w-full bg-bg-3 dark:bg-d-bg-3 rounded-sm border-2 border-dashed border-fg-2" bind:value={message} required/></div>
     <div><input class="bg-bg-3 w-full rounded-lg border-2 border-fg-2" type="submit" value="send" /></div>
-    <p class="text-sm text-right" on:mouseenter={() => toggle(true)} on:mouseleave={() => toggle(false)}>how does this form works<Dots character="?" min={2} max={3} />
+    <p class="my-2 text-sm text-right" on:mouseenter={() => toggle(true)} on:mouseleave={() => toggle(false)}>how does this form works<Dots character="?" min={2} max={3} />
       { #if showHow }
-        <p class="text-right">
+        <p class="text-right" in:slide out:fade>
           Thanks to an <a href="https://aws.amazon.com/lambda/" target="_blank">AWS Lambda function</a> written in Go, an API request is sent to <a href="https://core.telegram.org/bots/api" target="_blank">Telegram as a bot</a> to trigger the sending of a message.
         </p>
       { /if }
